949cca7df066680a6c1b2b3fa68ea5f873814569
Vikms <vms1995@hotmail.com>
2022-01-04 22:03:01 +0100

    .

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-
-const container = document.getElementById("container");
+const containerReference = document.getElementById("container");
 
 function makeRows(rows,cols)
 {
-    container.style.setProperty('--grid-rows',rows);
-    container.style.setProperty('--grid-cols',cols);
+    containerReference.style.setProperty('--grid-rows',rows);
+    containerReference.style.setProperty('--grid-cols',cols);
     for(c = 0; c < (rows * cols); c++)
-    {
+    {  
         let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
+        containerReference.appendChild(cell).className = "grid-item";
     };
 };
 
 makeRows(16,16);
+
+let divReference = document.querySelector(".grid-item");
+
+divReference.addEventListener('click', function(e){
+    e.target.style.color = "purple";
+    console.log(e.target.style.color);
+});
