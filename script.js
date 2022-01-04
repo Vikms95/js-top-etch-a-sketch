8cee32b148518ee37b8f81de87be2ffc210e3e81
Vikms <vms1995@hotmail.com>
2022-01-04 12:12:08 +0100

    Creates grid layout

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const bodyReference = document.querySelector('html');
-document.createElement('div');
+
+const container = document.getElementById("container");
+
+function makeRows(rows,cols)
+{
+    container.style.setProperty('--grid-rows',rows);
+    container.style.setProperty('--grid-cols',cols);
+    for(c = 0; c < (rows * cols); c++)
+    {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16,16);
