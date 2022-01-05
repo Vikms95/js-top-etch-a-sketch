68cda82f3453425445d44919151297e086f8e659
Blamak <vlasherassanchez@gmail.com>
2022-01-05 03:31:58 +0100

    Adds event listeners to grid, changing class on event which has a black background set

---
+++ notes.md
@@ -0,0 +1,4 @@
+//PSEUDO
+Get container reference and add the amount of rows and cols specified with appendChild. Give it "grid-item" class name.
+
+Get reference as a NodeList of all the elements inside the container div, which go after the class name "grid-item"
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const containerReference = document.getElementById("container");
-
 function makeRows(rows,cols)
 {
     containerReference.style.setProperty('--grid-rows',rows);
@@ -11,11 +9,26 @@ function makeRows(rows,cols)
     };
 };
 
+const containerReference = document.getElementById("container");
+
 makeRows(16,16);
 
-let divReference = document.querySelector(".grid-item");
+//-----
 
-divReference.addEventListener('click', function(e){
-    e.target.style.color = "purple";
-    console.log(e.target.style.color);
-});
+function addEventListenerNodeList(nodeList,event)
+{
+    for (var i = 0, len = nodeList.length; i < len; i++)
+    {   
+        nodeList[i].addEventListener(event, (event) => {
+            event.target.className = "grid-item-black";
+        });
+    };
+};
+
+let cellReference = document.querySelectorAll(".grid-item");
+// console.log(cellReference);
+addEventListenerNodeList(cellReference, 'mouseover');
+
+
+
+
